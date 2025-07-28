72af927470ba56c701bf3c4bffeae4be37438bbb
Martin Wang <pelkin.dev@gmx.de>
2025-07-28 17:40:51 +0200

    update tests

---
+++ src/test/java/com/xomstudio/DriveLogBook/DriveLogBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xomstudio.DriveLogBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveLogBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
