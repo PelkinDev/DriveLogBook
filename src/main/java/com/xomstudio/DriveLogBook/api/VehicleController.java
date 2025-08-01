19f6e684b8c6e3e128abd86f76e1867485ef1860
Martin Wang <pelkin.dev@gmx.de>
2025-08-01 13:31:55 +0200

    created class VehicleController

---
+++ src/main/java/com/xomstudio/DriveLogBook/api/VehicleController.java
@@ -0,0 +1,38 @@
+package com.xomstudio.DriveLogBook.api;
+
+import com.xomstudio.DriveLogBook.infrastructure.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/vehicle")
+public class VehicleController {
+
+    private final VehicleService vehicleService;
+
+    @Autowired
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
+    @GetMapping
+    public List<Vehicle> getVehicles(){
+        return vehicleService.getVehicles();
+    }
+
+    @PostMapping
+    public void addNewVehicle(@RequestBody Vehicle vehicle){
+        vehicleService.addNewVehicle(vehicle);
+    }
+
+    @DeleteMapping(path = "{vehicleId}")
+    public void deleteVehicle(@PathVariable("vehicleId")Long vehicleId){
+        vehicleService.deleteVehicle(vehicleId);
+    }
+
+
+
+
+}
