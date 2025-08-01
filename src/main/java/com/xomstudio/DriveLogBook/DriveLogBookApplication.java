820258427313e7b91ec2b4eb34a8def4b8eabeb3
Martin Wang <pelkin.dev@gmx.de>
2025-08-01 13:56:34 +0200

    removen temporary Vehicle instance

--- src/main/java/com/xomstudio/DriveLogBook/DriveLogBookApplication.java
+++ src/main/java/com/xomstudio/DriveLogBook/DriveLogBookApplication.java
@@ -1,7 +1,5 @@
 package com.xomstudio.DriveLogBook;
 
-import com.xomstudio.DriveLogBook.api.Fuel;
-import com.xomstudio.DriveLogBook.domain.Vehicle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,13 +9,6 @@ import java.time.*;
 public class DriveLogBookApplication {
 
 	public static void main(String[] args) {
-
-		Vehicle v1 = new Vehicle("FR-XM 188", LocalDate.of(2019, Month.DECEMBER, 22), 63000, Fuel.GASOLINE);
-		Vehicle v2 = new Vehicle("FR-OO 1234", LocalDate.of(2000, Month.JANUARY, 2), 463000, Fuel.DIESEL);
-
-		System.out.println(v1.toString());
-		System.out.println(v2.toString());
-
 
 		SpringApplication.run(DriveLogBookApplication.class, args);
 	}
