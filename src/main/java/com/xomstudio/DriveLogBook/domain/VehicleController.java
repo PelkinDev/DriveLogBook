99f623c3f41b009b05c332ebd9346ee76ad398af
Martin Wang <pelkin.dev@gmx.de>
2025-10-13 12:42:56 +0200

    changed to ResponseEntity

--- src/main/java/com/xomstudio/DriveLogBook/domain/VehicleController.java
+++ src/main/java/com/xomstudio/DriveLogBook/domain/VehicleController.java
@@ -1,38 +1,54 @@
 package com.xomstudio.DriveLogBook.domain;
 
 import com.xomstudio.DriveLogBook.api.VehicleService;
+import com.xomstudio.DriveLogBook.infrastructure.dto.VehicleDTO;
+import com.xomstudio.DriveLogBook.infrastructure.dto.VehicleMapper;
 import com.xomstudio.DriveLogBook.infrastructure.entities.VehicleEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "api/v1/vehicle")
 public class VehicleController {
 
     private final VehicleService vehicleService;
+    private VehicleMapper<VehicleEntity, VehicleDTO> vehicleMapper;
 
     @Autowired
-    public VehicleController(VehicleService vehicleService) {
+    public VehicleController(VehicleService vehicleService, VehicleMapper<VehicleEntity, VehicleDTO> vehicleMapper) {
         this.vehicleService = vehicleService;
+        this.vehicleMapper = vehicleMapper;
     }
 
     @GetMapping
-    public List<VehicleEntity> getVehicles(){
-        return vehicleService.getVehicles();
+    public List<VehicleDTO> getVehicles(){
+        List<VehicleEntity> vehicles = vehicleService.getVehicles();
+        return vehicles.stream()
+                .map(vehicleMapper::mapFromVehicleEntityToVehicleDTO)
+                .collect(Collectors.toList());
     }
+
 
     @GetMapping(path = "{vehicleId}")
     public Optional<VehicleEntity> getVehicleById(@PathVariable("vehicleId") Long vehicleId){
         return vehicleService.getVehicleById(vehicleId);
     }
 
+
     @PostMapping
-    public void addNewVehicle(@RequestBody VehicleEntity vehicle){
-        vehicleService.addNewVehicle(vehicle);
+    public ResponseEntity<VehicleDTO> addNewVehicles(@RequestBody VehicleDTO vehicleDTO) {
+        VehicleEntity vehicleEntity = vehicleMapper.mapFromVehicleDTOToVehicleEntity(vehicleDTO);
+        vehicleService.addNewVehicle(vehicleEntity);
+//        VehicleEntity savedVehicleEntity = vehicleService.addNewVehicle(vehicleEntity);
+        return new ResponseEntity<>(vehicleMapper.mapFromVehicleEntityToVehicleDTO(vehicleEntity), HttpStatus.CREATED);
     }
+
 
     @DeleteMapping(path = "{vehicleId}")
     public void deleteVehicle(@PathVariable("vehicleId")Long vehicleId){
