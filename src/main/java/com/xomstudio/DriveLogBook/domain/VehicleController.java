06e821537b80947fdbae2f045d7d308790354a4f
Martin Wang <pelkin.dev@gmx.de>
2025-10-16 09:20:35 +0200

    add interface VehicleService add partialUpdate method

--- src/main/java/com/xomstudio/DriveLogBook/domain/VehicleController.java
+++ src/main/java/com/xomstudio/DriveLogBook/domain/VehicleController.java
@@ -1,8 +1,9 @@
 package com.xomstudio.DriveLogBook.domain;
 
-import com.xomstudio.DriveLogBook.api.VehicleService;
+import com.xomstudio.DriveLogBook.api.VehicleServiceImpl;
 import com.xomstudio.DriveLogBook.infrastructure.dto.VehicleDTO;
-import com.xomstudio.DriveLogBook.infrastructure.dto.VehicleMapper;
+import com.xomstudio.DriveLogBook.infrastructure.dto.Mapper;
+import com.xomstudio.DriveLogBook.infrastructure.dto.VehicleMapperImpl;
 import com.xomstudio.DriveLogBook.infrastructure.entity.VehicleEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,42 +18,55 @@ import java.util.stream.Collectors;
 @RequestMapping(path = "api/v1/vehicle")
 public class VehicleController {
 
-    private final VehicleService vehicleService;
-    private VehicleMapper<VehicleEntity, VehicleDTO> vehicleMapper;
+    private final VehicleServiceImpl vehicleServiceImpl;
+    private Mapper<VehicleEntity, VehicleDTO> mapper;
 
     @Autowired
-    public VehicleController(VehicleService vehicleService, VehicleMapper<VehicleEntity, VehicleDTO> vehicleMapper) {
-        this.vehicleService = vehicleService;
-        this.vehicleMapper = vehicleMapper;
+    public VehicleController(VehicleServiceImpl vehicleServiceImpl, Mapper<VehicleEntity, VehicleDTO> mapper) {
+        this.vehicleServiceImpl = vehicleServiceImpl;
+        this.mapper = mapper;
     }
 
     @GetMapping
     public List<VehicleDTO> getVehicles(){
-        List<VehicleEntity> vehicles = vehicleService.getVehicles();
+        List<VehicleEntity> vehicles = vehicleServiceImpl.getVehicles();
         return vehicles.stream()
-                .map(vehicleMapper::mapFromVehicleEntityToVehicleDTO)
+                .map(mapper::mapFromEntityToDTO)
                 .collect(Collectors.toList());
     }
 
 
     @GetMapping(path = "{vehicleId}")
     public Optional<VehicleEntity> getVehicleById(@PathVariable("vehicleId") Long vehicleId){
-        return vehicleService.getVehicleById(vehicleId);
+        return vehicleServiceImpl.getVehicleById(vehicleId);
     }
 
 
     @PostMapping
     public ResponseEntity<VehicleDTO> addNewVehicles(@RequestBody VehicleDTO vehicleDTO) {
-        VehicleEntity vehicleEntity = vehicleMapper.mapFromVehicleDTOToVehicleEntity(vehicleDTO);
-        vehicleService.addNewVehicle(vehicleEntity);
+        VehicleEntity vehicleEntity = mapper.mapFromDTOToEntity(vehicleDTO);
+        vehicleServiceImpl.addNewVehicle(vehicleEntity);
 //        VehicleEntity savedVehicleEntity = vehicleService.addNewVehicle(vehicleEntity);
-        return new ResponseEntity<>(vehicleMapper.mapFromVehicleEntityToVehicleDTO(vehicleEntity), HttpStatus.CREATED);
+        return new ResponseEntity<>(mapper.mapFromEntityToDTO(vehicleEntity), HttpStatus.CREATED);
+    }
+
+    @PatchMapping(path = "{vehicleId}")
+    public ResponseEntity<VehicleDTO> partialUpdate(@PathVariable("vehicleId") Long vehicleId, @RequestBody VehicleDTO vehicleDTO){
+
+        if(!vehicleServiceImpl.isExists(vehicleId)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        VehicleEntity vehicleEntity = mapper.mapFromDTOToEntity(vehicleDTO);
+        VehicleEntity updatedVehicle = vehicleServiceImpl.partialUpdate(vehicleId, vehicleEntity);
+        return new ResponseEntity<>(mapper.mapFromEntityToDTO(updatedVehicle), HttpStatus.OK);
     }
 
 
     @DeleteMapping(path = "{vehicleId}")
     public void deleteVehicle(@PathVariable("vehicleId")Long vehicleId){
-        vehicleService.deleteVehicle(vehicleId);
+        vehicleServiceImpl.deleteVehicle(vehicleId);
     }
 
 }
+
