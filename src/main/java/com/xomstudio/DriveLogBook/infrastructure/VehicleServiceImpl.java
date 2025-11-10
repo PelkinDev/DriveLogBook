b723bcfe86fabf4e4cda04a0e5f32ffda0153e19
Martin Wang <pelkin.dev@gmx.de>
2025-11-10 16:35:57 +0100

    add Slf4j logger

--- src/main/java/com/xomstudio/DriveLogBook/infrastructure/VehicleServiceImpl.java
+++ src/main/java/com/xomstudio/DriveLogBook/infrastructure/VehicleServiceImpl.java
@@ -3,12 +3,14 @@ package com.xomstudio.DriveLogBook.infrastructure;
 import com.xomstudio.DriveLogBook.api.VehicleRepository;
 import com.xomstudio.DriveLogBook.api.VehicleService;
 import com.xomstudio.DriveLogBook.infrastructure.entity.VehicleEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class VehicleServiceImpl implements VehicleService {
 
@@ -32,7 +34,8 @@ public class VehicleServiceImpl implements VehicleService {
     public Optional<VehicleEntity> getVehicleById(Long vehicleId){
         boolean vehicleOpt = vehicleRepository.existsById(vehicleId);
         if(!vehicleOpt){
-            throw new IllegalStateException("vehicle with id " + vehicleId + " not exists.");
+            log.warn("vehicle with ID: {} not exists", vehicleId);
+//            throw new IllegalStateException("vehicle with id " + vehicleId + " not exists.");
         }
         return vehicleRepository.findById(vehicleId);
     }
@@ -41,8 +44,10 @@ public class VehicleServiceImpl implements VehicleService {
     public void addNewVehicle(VehicleEntity vehicle){
         Optional<VehicleEntity> vehicleOptional = vehicleRepository.findVehicleByCarLicensePlate(vehicle.getCarLicensePlate());
         if(vehicleOptional.isPresent()){
+            log.error("the vehicle with this license plate already exists");
             throw new IllegalStateException("the vehicle with this license plate already exists");
         }
+        log.info("new vehicle was created");
         vehicleRepository.save(vehicle);
     }
 
@@ -53,24 +58,26 @@ public class VehicleServiceImpl implements VehicleService {
         return vehicleRepository.findById(id).map(existingVehicle -> {
             Optional.ofNullable(vehicleEntity.getCarLicensePlate()).ifPresent((existingVehicle::setCarLicensePlate));
             Optional.ofNullable(vehicleEntity.getFirstRegistration()).ifPresent((existingVehicle::setFirstRegistration));
-            Optional.ofNullable(vehicleEntity.getMileage()).ifPresent((existingVehicle::setMileage));
+            Optional.of(vehicleEntity.getMileage()).ifPresent((existingVehicle::setMileage));
             Optional.ofNullable(vehicleEntity.getVin()).ifPresent((existingVehicle::setVin));
             Optional.ofNullable(vehicleEntity.getCarBrand()).ifPresent((existingVehicle::setCarBrand));
             Optional.ofNullable(vehicleEntity.getCarModel()).ifPresent((existingVehicle::setCarModel));
             Optional.ofNullable(vehicleEntity.getCarColor()).ifPresent((existingVehicle::setCarColor));
-            Optional.ofNullable(vehicleEntity.getEnginePower()).ifPresent((existingVehicle::setEnginePower));
+            Optional.of(vehicleEntity.getEnginePower()).ifPresent((existingVehicle::setEnginePower));
             Optional.ofNullable(vehicleEntity.getPetrol()).ifPresent((existingVehicle::setPetrol));
-
+            log.info("Vehicle with ID: {} was changed", id);
             return vehicleRepository.save(existingVehicle);
-        }).orElseThrow(() -> new RuntimeException("Vehicle does not exist"));
-
+        }).orElseThrow(() -> new IllegalStateException("vehicle with id " + id + " not exists"));
     }
+
 
     public void deleteVehicle(Long vehicleId){
         boolean vehicleExist = vehicleRepository.existsById(vehicleId);
         if(!vehicleExist){
-            throw new IllegalStateException("vehicle with id " + vehicleId + " not exists.");
+//            throw new IllegalStateException("vehicle with id " + vehicleId + " not exists.");
+            log.warn("Vehicle with ID: {} not exists", vehicleId);
         }
+        log.info("Vehicle with ID: {} deleted", vehicleId);
         vehicleRepository.deleteById(vehicleId);
     }
 
