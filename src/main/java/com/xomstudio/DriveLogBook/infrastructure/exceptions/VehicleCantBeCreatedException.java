2aa34ec0f12f2e2b3077953e16d0c93d3faf8ad3
Martin Wang <pelkin.dev@gmx.de>
2026-03-06 10:59:04 +0100

    update exceptions

--- src/main/java/com/xomstudio/DriveLogBook/infrastructure/exceptions/VehicleCantBeCreatedException.java
+++ src/main/java/com/xomstudio/DriveLogBook/infrastructure/exceptions/VehicleCantBeCreatedException.java
@@ -1,5 +1,9 @@
 package com.xomstudio.DriveLogBook.infrastructure.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class VehicleCantBeCreatedException extends DriveLogBookException {
 
     public VehicleCantBeCreatedException() {
--- src/main/java/com/xomstudio/DriveLogBook/infrastructure/exceptions/VehicleNotFoundException.java
+++ src/main/java/com/xomstudio/DriveLogBook/infrastructure/exceptions/VehicleNotFoundException.java
@@ -1,5 +1,9 @@
 package com.xomstudio.DriveLogBook.infrastructure.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class VehicleNotFoundException extends DriveLogBookException {
 
     public VehicleNotFoundException() {
